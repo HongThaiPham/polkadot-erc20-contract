3023935bc51578960af9696d5e6b9b173b772983
Pham Hong Thai <hongthaipro@gmail.com>
2021-10-12 13:30:30 +0700

    Supporting Approvals and Transfer From

--- lib.rs
+++ lib.rs
@@ -11,6 +11,10 @@ mod erc20 {
         total_supply: Balance,
         /// The balance of each user.
         balances: ink_storage::collections::HashMap<AccountId, Balance>,
+        /// Approval spender on behalf of the message's sender.
+        //  ACTION: Add an `allowances` storage item. It should be a
+        //         `HashMap` from `(AccountId, AccountId)` to `Balance`
+        allowances : ink_storage::collections::HashMap<(AccountId,AccountId), Balance>
     }
 
     #[ink(event)]
@@ -23,6 +27,19 @@ mod erc20 {
         from: Option<AccountId>,
         #[ink(topic)]
         to: Option<AccountId>,
+        value: Balance
+    }
+
+    // ACTION: Add an `Approval` event
+    //         It should emit the following:
+    //         * `owner` as an `AccountId`
+    //         * `spender` as an `AccountId`
+    //         * `value` as a `Balance`
+
+    #[ink(event)]
+    pub struct Approval {
+        owner: AccountId,
+        spender: AccountId,
         value: Balance
     }
 
@@ -48,7 +65,8 @@ mod erc20 {
             
             Self {
                 total_supply: initial_supply, 
-                balances: _balances
+                balances: _balances,
+                allowances: ink_storage::collections::HashMap::new(),
             }
         }
 
@@ -63,6 +81,55 @@ mod erc20 {
             // ACTION: Return the balance of `owner`
             //   HINT: Use `balance_of_or_zero` to get the `owner` balance
             self.balance_of_or_zero(&owner)
+        }
+
+        #[ink(message)]
+        pub fn approve(&mut self, spender: AccountId, value: Balance) -> bool {
+            // ACTION: Get the `self.env().caller()` and store it as the `owner`
+            let owner = self.env().caller();
+            // ACTION: Insert the new allowance into the `allowances` HashMap
+            //   HINT: The key tuple is `(owner, spender)`
+            self.allowances.insert((owner, spender), value);
+            // ACTION: `emit` the `Approval` event you created using these values
+            // ACTION: Return true if everything was successful
+            self.env().emit_event(
+                Approval {
+                    owner: owner,
+                    spender: spender,
+                    value: value
+                }
+            );
+            true
+        }
+
+        #[ink(message)]
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            // ACTION: Create a getter for the `allowances` HashMap
+            //   HINT: Take a look at the getters above if you forget the details
+            // ACTION: Return the `allowance` value
+            self.allowance_of_or_zero(&owner, &spender)
+        }
+
+        #[ink(message)]
+        pub fn transfer_from(&mut self, from: AccountId, to: AccountId, value: Balance) -> bool {
+            // ACTION: Get the allowance for `(from, self.env().caller())` using `allowance_of_or_zero`
+            let _caller = self.env().caller();
+            let allowance = self.allowance_of_or_zero(&from, &_caller);
+            // ACTION: `if` the `allowance` is less than the `value`, exit early and return `false`
+            if allowance < value {
+                return false;
+            }
+            // ACTION: `insert` the new allowance into the map for `(from, self.env().caller())`
+            let transfer_result = self.transfer_from_to(from, to, value);
+             // Check `transfer_result` because `from` account may not have enough balance
+            //   and return false.
+            if !transfer_result {
+                return false
+            }
+            // ACTION: Finally, call the `transfer_from_to` for `from` and `to`
+            self.allowances.insert((from, _caller), allowance - value);
+            // ACTION: Return true if everything was successful
+            true
         }
 
         #[ink(message)]
@@ -107,6 +174,18 @@ mod erc20 {
             // ACTION: Return the balance
             *self.balances.get(owner).unwrap_or(&0)
         }
+
+        fn allowance_of_or_zero(&self, owner: &AccountId, spender: &AccountId) -> Balance {
+            // ACTION: `get` the `allowances` of `(owner, spender)` and `unwrap_or` return `0`.
+            // If you are new to Rust, you may wonder what's the deal with all the asterisks and
+            // ampersends.
+            //
+            // In brief, using `&` if we want to get the address of a value (aka reference of the
+            // value), and using `*` if we have the reference of a value and want to get the value
+            // back (aka dereferencing).
+            // To read more: https://doc.rust-lang.org/book/ch04-02-references-and-borrowing.html
+            *self.allowances.get(&(*owner, *spender)).unwrap_or(&0)
+        }
     }
 
     #[cfg(test)]
@@ -136,6 +215,31 @@ mod erc20 {
             assert!(contract.transfer(AccountId::from([0x0; 32]), 10));
             assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 10);
             assert!(!contract.transfer(AccountId::from([0x0; 32]), 100));
+        }
+
+        #[ink::test]
+        fn transfer_from_works() {
+            let mut contract = Erc20::new(100);
+            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+            contract.approve(AccountId::from([0x1; 32]), 20);
+            contract.transfer_from(AccountId::from([0x1; 32]), AccountId::from([0x0; 32]), 10);
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 10);
+        }
+
+        #[ink::test]
+        fn allowances_works() {
+            let mut contract = Erc20::new(100);
+            assert_eq!(contract.balance_of(AccountId::from([0x1; 32])), 100);
+            contract.approve(AccountId::from([0x1; 32]), 200);
+            assert_eq!(contract.allowance(AccountId::from([0x1; 32]), AccountId::from([0x1; 32])), 200);
+
+            assert!(contract.transfer_from(AccountId::from([0x1; 32]), AccountId::from([0x0; 32]), 50));
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 50);
+            assert_eq!(contract.allowance(AccountId::from([0x1; 32]), AccountId::from([0x1; 32])), 150);
+
+            assert!(!contract.transfer_from(AccountId::from([0x1; 32]), AccountId::from([0x0; 32]), 100));
+            assert_eq!(contract.balance_of(AccountId::from([0x0; 32])), 50);
+            assert_eq!(contract.allowance(AccountId::from([0x1; 32]), AccountId::from([0x1; 32])), 150);
         }
     }
 }
